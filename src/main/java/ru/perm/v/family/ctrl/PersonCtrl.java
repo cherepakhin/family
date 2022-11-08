67764433d1eebd194da0fb085f1a5a224a2abef8
Черепахин Василий <vasi.che@gmail.com>
2022-11-08 14:34:31 +0500

    Fix test import

---
+++ src/main/java/ru/perm/v/family/ctrl/PersonCtrl.java
@@ -0,0 +1,43 @@
+package ru.perm.v.family.ctrl;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Контроллер семьи
+ */
+@RestController
+@RequestMapping("/person")
+@Slf4j
+@Validated
+@AllArgsConstructor
+@Api(tags = "Person Controller")
+public class PersonCtrl {
+
+    /**
+     * Эхо контроллер для теста
+     *
+     * @param mes сообщение для теста. Любая строка.
+     * @return принятое сообщение
+     */
+    @GetMapping("/echo/{mes}")
+    @ApiOperation(value = "Простой echo запрос", notes = "Для проверки жив/нет")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Все OK (простая демонстрация аннотации @ApiResponse)")
+    })
+    public String echo(
+        @ApiParam("любая тестовая строка. Должна вернуться в ответе.") @PathVariable String mes) {
+        log.info("Request \"echo\" received: {}", mes);
+        return mes;
+    }
+}
--- src/test/java/ru/perm/v/family/ctrl/PersonCtrlTest.java
+++ src/test/java/ru/perm/v/family/ctrl/PersonCtrlTest.java
@@ -1,6 +1,9 @@
 package ru.perm.v.family.ctrl;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,26 +13,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {FamilyCtrl.class})
+@ContextConfiguration(classes = {PersonCtrl.class})
 @WebMvcTest
-public class FamilyCtrlTest {
-
+class PersonCtrlTest {
     @Autowired
     private MockMvc mockMvc;
-    private final String root = "/family";
+    private final String root = "/person";
 
     @Test
     void echo() throws Exception {
         String mes = "aaa";
         MvcResult result = mockMvc.perform(get(root + "/echo/" + mes)
-                        .contentType(MediaType.TEXT_PLAIN)
-                        .accept(MediaType.ALL))
-                .andExpect(status().isOk())
-                .andReturn();
+            .contentType(MediaType.TEXT_PLAIN)
+            .accept(MediaType.ALL))
+            .andExpect(status().isOk())
+            .andReturn();
         String res = result.getResponse().getContentAsString();
         assertEquals(mes, res);
     }
