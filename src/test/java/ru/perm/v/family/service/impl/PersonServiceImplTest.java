b6fac507243abb1cb7592513edf8d1d9088ee3bc
Черепахин Василий Геннадьевич <vasi.che@gmail.com>
2022-11-15 16:10:37 +0500

    delete mock from PersonServiceImplTest

--- src/test/java/ru/perm/v/family/service/impl/PersonServiceImplTest.java
+++ src/test/java/ru/perm/v/family/service/impl/PersonServiceImplTest.java
@@ -25,23 +25,20 @@ class PersonServiceImplTest {
         expectedPerson.setId(0L);
         expectedPerson.setName("-");
         when(mockPersonRepository.getReferenceById(0L)).thenReturn(expectedPerson);
-        PersonService personServiceMock = new PersonServiceImpl(mockPersonRepository);
-        PersonEntity person = personServiceMock.getById(0L);
+        PersonEntity person = personService.getById(0L);
         assertEquals(expectedPerson,person);
     }
 
     @Test
     void getByIdMockRepository() {
-        PersonRepository mockRepository = mock(PersonRepository.class);
-        personService = new PersonServiceImpl(mockRepository);
         Long ID= 0L;
         PersonEntity expectedPerson = new PersonEntity();
         expectedPerson.setId(ID);
         expectedPerson.setName("-");
-        when(mockRepository.getReferenceById(ID)).thenReturn(expectedPerson);
+        when(mockPersonRepository.getReferenceById(ID)).thenReturn(expectedPerson);
 
         PersonEntity actualPerson = personService.getById(ID);
         assertEquals(expectedPerson,actualPerson);
-        verify(mockRepository, times(1)).getReferenceById(ID);
+        verify(mockPersonRepository, times(1)).getReferenceById(ID);
     }
 }
